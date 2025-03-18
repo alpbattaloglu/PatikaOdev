7fe41bf5a8f38edcfa63d891e478a77e7682c859
alpbattaloglu <ahmetalpb@gmail.com>
2025-03-18 23:04:27 +0300

    MatrixTranspose.java

--- MatrixTranspose.java
+++ MatrixTranspose.java
@@ -3,7 +3,7 @@ public class MatrixTranspose {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        // Kullanıcıdan matris boyutlarını al
+        
         System.out.print("Satır sayısını girin: ");
         int satir = scanner.nextInt();
         System.out.print("Sütun sayısını girin: ");
@@ -12,7 +12,7 @@ public class MatrixTranspose {
         int[][] matris = new int[satir][sutun];
         int[][] transpoz = new int[sutun][satir];
         
-        // Kullanıcıdan matrisi al
+      
         System.out.println("Matris elemanlarını girin:");
         for (int i = 0; i < satir; i++) {
             for (int j = 0; j < sutun; j++) {
@@ -21,23 +21,23 @@ public class MatrixTranspose {
             }
         }
         
-        // Transpozunu al
+ 
         for (int i = 0; i < satir; i++) {
             for (int j = 0; j < sutun; j++) {
                 transpoz[j][i] = matris[i][j];
             }
         }
         
-        // Orijinal matrisi yazdır
         System.out.println("Orijinal Matris:");
         printMatrix(matris);
         
-        // Transpoz matrisi yazdır
+    
         System.out.println("Transpoz Matris:");
         printMatrix(transpoz);
     }
     
-    // Matrisi ekrana yazdıran yardımcı fonksiyon
+
+    
     public static void printMatrix(int[][] matris) {
         for (int[] satir : matris) {
             for (int eleman : satir) {
