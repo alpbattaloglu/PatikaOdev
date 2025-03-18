0095ed23dc28ca2cad81262ac1bd781842d1d33f
alpbattaloglu <ahmetalpb@gmail.com>
2025-03-18 23:00:46 +0300

    Add files via upload

---
+++ Maas/src/Employee.java
@@ -0,0 +1,52 @@
+public class Employee {
+    private String name; // Çalışanın adı
+    private double salary; // Çalışanın maaşı
+    private int workHours; // Çalışanın çalışma saati
+    private int hireYear; // Çalışanın işe başlama yılı
+    
+    public Employee(String name, double salary, int workHours, int hireYear) {
+        this.name = name;
+        this.salary = salary;
+        this.workHours = workHours;
+        this.hireYear = hireYear;
+    }
+
+    public double tax() {
+        if (salary < 1000) {
+            return 0;
+        } else {
+            return salary * 0.03;
+        }
+    }
+
+    public double bonus() {
+        if (workHours > 40) {
+            return (workHours - 40) * 30;
+        } else {
+            return 0;
+        }
+    }
+
+    public double raiseSalary() {   
+        int year = 2021 - hireYear;
+        if (year < 10) {
+            return salary * 0.05;
+        } else if (year >= 10 && year < 20) {
+            return salary * 0.10;
+        } else {
+            return salary * 0.15;
+        }
+    }
+
+    public String toString() {
+        return "Çalışan Bilgileri\n" +
+               "-----------------\n" +
+               "Adı: " + name + "\n" +
+               "Maaşı: " + salary + " TL\n" +
+               "Çalışma Saati: " + workHours + "\n" +
+               "Başlangıç Yılı: " + hireYear + "\n" +
+               "Vergi: " + tax() + " TL\n" +
+               "Bonus: " + bonus() + " TL\n" +
+               "Maaş Artışı: " + raiseSalary() + " TL";
+    }
+} 
